0e23cf3f27699a83658c5a2f10522acfdf9f69d7
ttwange <ttwange2@gmail.com>
2023-07-15 12:30:26 +0300

    login functionality in views

--- website/views.py
+++ website/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth import login,logout, authenticate
 from django.contrib import messages
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
