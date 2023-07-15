f9b209ea7b5f04f116d53ae4ffb193d2ff02ddb5
ttwange <ttwange2@gmail.com>
2023-07-15 11:09:45 +0300

    Setting up complete

--- mydb.py
+++ mydb.py
@@ -1,1 +1,20 @@
 #install Mysql on your computer
+#install mysql-connector or  mysql-connector-python
+
+
+import  mysql.connector
+
+dataBase =  mysql.connector.connect(
+    host = '',
+    user = '',
+    passwd = ''
+)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+#create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+
+print("Database created")
