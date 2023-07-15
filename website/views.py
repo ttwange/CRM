167684410673b80baef9b64fe3cf609f85b93def
ttwange <ttwange2@gmail.com>
2023-07-15 12:25:54 +0300

    import required packages for login, logout and authenticate

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login,logout, authenticate
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
