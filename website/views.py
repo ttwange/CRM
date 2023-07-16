c4c393aab5e1669cf97fe8f2bb65323f7a7aef63
ttwange <ttwange2@gmail.com>
2023-07-17 00:40:49 +0300

    Working onadding new records functionality

--- website/views.py
+++ website/views.py
@@ -71,7 +71,7 @@ def delete_record(request, pk):
         
 def add_record(request):
     form = AddRecordForm(request.POST or None)
-    if request.is_authenticated:
+    if request.user.is_authenticated:
         if request.method == "POST":
             if form.is_valid():
                 add_record = form.save()
