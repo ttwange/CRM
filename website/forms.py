f489d9d6609a3f3a70131de6b5cfbbe265483510
ttwange <ttwange2@gmail.com>
2023-07-17 00:27:01 +0300

    Working onadding new records functionality

--- website/forms.py
+++ website/forms.py
@@ -54,11 +54,11 @@ class AddRecordForm(forms.ModelForm):
 #create add record form
 
 class AddRecordForm(forms.ModelForm):
-      first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrsv={"placeholder":"First Name", "class":"form-control"})
-      last_name
-      email
-      phone
-      address
-      city
-      state
-      zipcode
+      first_name = forms.CharField(required=True, widget = forms.widgets.TextInput(attrsv={"placeholder":"First Name", "class":"form-control"}),label="")
+      last_name = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Last Name", "class":"form-control"}),label="")
+      email = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Email", "class":"form-control"}),label="")
+      phone = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Phone", "class":"form-control"}),label="")
+      address = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Address", "class":"form-control"}),label="")
+      city = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"City", "class":"form-control"}),label="")
+      state = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"State", "class":"form-control"}),label="")
+      zipcode = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Zipcode", "class":"form-control"}),label="")
