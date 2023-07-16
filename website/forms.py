3a4fe7aacb104b4a23234f6bc38832c5f7c8952e
ttwange <ttwange2@gmail.com>
2023-07-16 15:04:49 +0300

    added records model

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms 
-from .models import Records
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}))
