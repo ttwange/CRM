dd838633c38ff706a9bf528e8a140edb9f5f952e
ttwange <ttwange2@gmail.com>
2023-07-16 11:48:05 +0300

    Conditional on whether the method is a post or get

--- website/views.py
+++ website/views.py
@@ -6,21 +6,20 @@ from django.contrib import messages
 # Create your views here.
 def home(request):
     #checking for log in
-    username =  request.POST['username']
-    password = request.POST['password']  
-    #authenticate
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        messages.success(request, "Successful log in!!")
-        return redirect('home')
+    if request.method == 'POST':
+      username =  request.POST['username']
+      password = request.POST['password']  
+      #authenticate
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+          login(request, user)
+          messages.success(request, "Successful log in!!")
+          return redirect('home')
+      else:
+          messages.success('Error loggin in!!')
+          return redirect('home')
     else:
-        messages.success('Error loggin in!!')
-        return redirect('home')
-    return render(request, 'home.html', {})
-
-def login_user(request):
-    pass
+        return render(request, 'home.html', {})
 
 def logout_user(request):
     pass
