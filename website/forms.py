ab03b7ee41ee2a1148a381c442cd39955cc9464f
ttwange <ttwange2@gmail.com>
2023-07-17 00:38:42 +0300

    Working onadding new records functionality

--- website/forms.py
+++ website/forms.py
@@ -54,7 +54,7 @@ class AddRecordForm(forms.ModelForm):
 #create add record form
 
 class AddRecordForm(forms.ModelForm):
-      first_name = forms.CharField(required=True, widget = forms.widgets.TextInput(attrsv={"placeholder":"First Name", "class":"form-control"}),label="")
+      first_name = forms.CharField(required=True, widget = forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}),label="")
       last_name = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Last Name", "class":"form-control"}),label="")
       email = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Email", "class":"form-control"}),label="")
       phone = forms.CharField(required=True, widget =forms.widgets.TextInput(attrsv={"placeholder":"Phone", "class":"form-control"}),label="")
