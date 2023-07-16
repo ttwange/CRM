da412a702bde5ebba7b7fcc1088994305f848a49
ttwange <ttwange2@gmail.com>
2023-07-16 14:44:37 +0300

    Adding registration functionality

--- website/forms.py
+++ website/forms.py
@@ -4,6 +4,6 @@ from django import forms
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}))
-    first_name = forms.CharField(label="", max_length="100" ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}))
-    last_name = forms.CharField(label="",)
+    first_name = forms.CharField(label="", max_length="100" ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="",max_length="100" ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
 
