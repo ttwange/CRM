b3b01434a9f10c442525f5d95a9b1143c42438bf
ttwange <ttwange2@gmail.com>
2023-07-16 15:42:48 +0300

    registration functionality

--- website/forms.py
+++ website/forms.py
@@ -10,7 +10,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','password1','password2')
+        fields = ('username','first_name','last_name','email','password1','password2')
 
     
     def __init__(self,*args, **kwargs):
