c458e347f5ed67336e4c80fcd2b9eaa59e083f69
ttwange <ttwange2@gmail.com>
2023-07-17 00:51:38 +0300

    Update record functinality

--- website/views.py
+++ website/views.py
@@ -89,3 +89,7 @@ def update_record(request, pk):
     if request.user.is_authenticated:
         current_record = Record.objects.get(id=pk) 
         form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+                form.save()
+                messages.success(request, "Record updated")
+                return redirect('home')
