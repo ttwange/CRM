8371d8108d9c432b5ab13991b59688c4630f8cc7
ttwange <ttwange2@gmail.com>
2023-07-16 14:33:04 +0300

    Adding registration functionality

--- website/forms.py
+++ website/forms.py
@@ -2,4 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms 
 
-
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput())
+    
